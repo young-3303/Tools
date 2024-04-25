47fd4e07687e25f31b456eb550aa8cd405a67609
young <55731744@qq.com>
2024-04-25 20:56:32 +0800

    init tool

---
+++ UseMvvm/ViewModel/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace UseMvvm.ViewModel
+{
+    public class MainViewModel : ObservableValidator
+    {
+
+    }
+}
