787acfa98e783dca290ecb40e30a7f8da50d0670
young <55731744@qq.com>
2024-04-26 10:52:25 +0800

    添加相关nuget包，处理进程相关逻辑

--- Tools/App.xaml.cs
+++ Tools/App.xaml.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using Tools.Utils;
 
 namespace Tools
 {
@@ -13,5 +15,21 @@ namespace Tools
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            Startup += (object sender, StartupEventArgs e) =>
+            {
+                // 使线程互斥，只有一个app实例
+                mutex = new Mutex(true, "name", out bool createdNew);
+                if(!createdNew)
+                {
+                    // 打开应用如果程序已启动，就激活窗口到最前面
+                    Common.ActivationThread();
+                    //如果有一样的实例就退出新开的app实例
+                    Environment.Exit(0);
+                }
+            };
+        }
+        private static Mutex mutex;
     }
 }
---
+++ Tools/Utils/Common.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tools.Utils
+{
+    internal class Common
+    {
+        /// <summary>
+        /// win32 api窗口最小化还原正常状态
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <param name="cmdShow"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int cmdShow);
+        /// <summary>
+        /// win32 api 将窗口置于前台
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+        /// <summary>
+        /// 激活线程
+        /// </summary>
+        public static void ActivationThread()
+        {
+            Process mainProces= Process.GetCurrentProcess();
+            Process[] processes = Process.GetProcessesByName(mainProces.ProcessName);
+            const int SW_RESTORE = 9;
+            foreach (var process in processes)
+            {
+                if(process.Id != mainProces.Id)
+                {
+                    ShowWindow(process.MainWindowHandle, SW_RESTORE);
+                    SetForegroundWindow(process.MainWindowHandle);
+                    break;
+                }
+            }
+        }
+    }
+}
