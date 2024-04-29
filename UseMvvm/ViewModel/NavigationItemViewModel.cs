ffdaa1d6f699748049d928fd9960eab452db34b1
young <55731744@qq.com>
2024-04-29 15:23:22 +0800

    添加uc

---
+++ UseMvvm/ViewModel/NavigationItemViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UseMvvm.ViewModel
+{
+    public class NavigationItemViewModel
+    {
+        public string Name { get; set; }
+        public object Content { get; set; }
+        // 可以添加更多项或行为
+    }
+}
